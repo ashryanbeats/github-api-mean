3ca51313a060b66d41fb0f4ba066183d3a08f167
Ash <ash@ashryanbeats.com>
2016-05-08 20:18:04 -0400

    Log out of GitHub by removing access_token.

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,4 +1,13 @@
-app.controller('HomeController', function($scope, $http, HomeFactory) {
+app.controller('HomeController', function($scope, $http, HomeFactory, $cookies, $state) {
+
+	$scope.access_token = $cookies.get('access_token');
+	$scope.loggedIn = $scope.access_token ? true : false;
+
+	$scope.logOut = function () {
+		$cookies.remove('access_token');
+		$state.reload();
+	}
+
 
 	$scope.getData = function () {
 		$http.get("/api/auth")
